3aed34f3faefdaca9c3ceafcae2c185a42acb69c
Grainne Donegan <grainnejdonegan@gmail.com>
2022-08-04 14:01:56 +0000

    add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown Game Type: ${gameType}`);
         throw `Unknown Game Type: ${gameType}. Aborting`;
@@ -81,6 +83,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === '-') {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === '/') {
+        return [operand1 / operand2, "division"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -123,6 +127,9 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivideQuestion() {
-
+function displayDivisionQuestion(operand1, operand2) {
+    let finalHigher = operand1 * operand2;
+    document.getElementById('operand1').textContent = finalHigher;
+    document.getElementById('operand2').textContent = operand1;
+    document.getElementById('operator').textContent = '/';
 }
